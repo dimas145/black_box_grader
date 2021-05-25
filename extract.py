de24f77895556a1eab7d60e3d06506d36e5f14a4
Bram Musuko Panjaitan <bram.musuko@gmail.com>
2021-05-25 21:14:51 +0700

    make tmp/src folder

--- extract.py
+++ extract.py
@@ -22,7 +22,7 @@ def extractTarGz(tarGzBase64, basePath):
     targzBin = base64.b64decode(tarGzBase64)
     tf = tarfile.open(fileobj=BytesIO(targzBin))
     print(tf.getmembers())
-
+    os.makedirs(basePath)
     for member in tf.getmembers():
         print(member.path)
         if member.isdir():
