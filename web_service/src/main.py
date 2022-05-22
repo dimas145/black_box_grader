5946cc04819a1e9248243b1ad428904063aa1a46
dimas145 <13518069@std.stei.itb.ac.id>
2022-05-22 21:51:15 +0700

    pull python image when starting app

--- web_service/src/main.py
+++ web_service/src/main.py
@@ -5,6 +5,11 @@ from flask_restful import Api
 from web_service.src.resources.health_check import HealthCheck
 from web_service.src.resources.black_box_grader import BlackBoxGrader
 
+import docker
+
+client = docker.from_env()
+IMAGE_NAME = "python:3.9-alpine"
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -13,4 +18,5 @@ api.add_resource(BlackBoxGrader, '/grade')
 
 
 if __name__ == '__main__':
+    client.images.pull(IMAGE_NAME)
     app.run(host="0.0.0.0")
